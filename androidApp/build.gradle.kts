9e848fc9f0d21265f4e6522cbf11192e0b9be0f7
vsbauer <vlad.bauer24@gmail.com>
2020-11-01 21:59:30 +0300

    Initial commit

---
+++ androidApp/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    id("kotlin-android-extensions")
+}
+group = "com.github.vsbauer"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    gradlePluginPortal()
+    google()
+    jcenter()
+    mavenCentral()
+}
+dependencies {
+    implementation(project(":shared"))
+    implementation("androidx.core:core-ktx:1.2.0")
+    implementation("androidx.appcompat:appcompat:1.1.0")
+    implementation("androidx.constraintlayout:constraintlayout:1.1.3")
+}
+android {
+    compileSdkVersion(30)
+    defaultConfig {
+        applicationId = "com.github.vsbauer.teapp"
+        minSdkVersion(24)
+        targetSdkVersion(30)
+        versionCode = 1
+        versionName = "1.0"
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+        }
+    }
+}
