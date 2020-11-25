d50bbd327cf8cad75a76aeb16e15590bb58113a2
VITYQ <vitaly.sorokin14@gmail.com>
2020-11-25 21:42:38 +0300

    Добавлены AndroidX библиотеки

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     kotlin("android")
     id("kotlin-android-extensions")
+    id("kotlin-android")
 }
 group = "com.github.vsbauer"
 version = "1.0-SNAPSHOT"
@@ -19,6 +20,9 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.2.0")
     implementation("androidx.constraintlayout:constraintlayout:2.0.4")
     implementation("com.yandex.android:maps.mobile:4.0.0-lite")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:${rootProject.extra["kotlin_version"]}")
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
+    implementation("androidx.fragment:fragment-ktx:1.2.5")
 }
 android {
     compileSdkVersion(30)
@@ -34,4 +38,10 @@ android {
             isMinifyEnabled = false
         }
     }
+    tasks {
+        withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+            kotlinOptions.jvmTarget = "1.8"
+        }
+    }
+    
 }
