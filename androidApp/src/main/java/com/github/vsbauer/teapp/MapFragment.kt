f36538b2cfcc62df38a8707a75347520220cdfbf
VITYQ <vitaly.sorokin14@gmail.com>
2020-11-25 21:26:02 +0300

    Добавлен MapFragment.kt Добавлена инициализация карты Карта при запуске переводится на СПб

---
+++ androidApp/src/main/java/com/github/vsbauer/teapp/MapFragment.kt
@@ -0,0 +1,54 @@
+package com.github.vsbauer.teapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.yandex.mapkit.Animation
+import com.yandex.mapkit.MapKitFactory
+import com.yandex.mapkit.geometry.Point
+import com.yandex.mapkit.map.CameraPosition
+import kotlinx.android.synthetic.main.fragment_map.*
+
+
+class MapFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        MapKitFactory.setApiKey("48d45a79-cbbd-4c13-9649-f3c003f692a6")
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_map, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        MapKitFactory.initialize(context)
+        mapview.map.move(
+            CameraPosition(Point(59.93863, 30.31413), 11.0f, 0.0f, 0.0f),
+            Animation(Animation.Type.SMOOTH, 0.toFloat()),
+            null)
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+        mapview.onStart()
+        MapKitFactory.getInstance().onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapview.onStop()
+        MapKitFactory.getInstance().onStop()
+    }
+
+}
