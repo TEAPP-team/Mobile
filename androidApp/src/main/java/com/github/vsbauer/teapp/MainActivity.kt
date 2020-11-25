636de1073ffc19dc7a55b093d4bba61aa01fe398
VITYQ <vitaly.sorokin14@gmail.com>
2020-11-25 21:27:08 +0300

    Ничего не изменилось

--- androidApp/src/main/java/com/github/vsbauer/teapp/MainActivity.kt
+++ androidApp/src/main/java/com/github/vsbauer/teapp/MainActivity.kt
@@ -1,5 +1,18 @@
 package com.github.vsbauer.teapp
 
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
 
-class   MainActivity : AppCompatActivity(R.layout.activity_main)
+class   MainActivity : AppCompatActivity(R.layout.activity_main){
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        Log.d("test", "test")
+
+    }
+}
