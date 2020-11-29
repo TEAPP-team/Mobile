8534eddbf1ea9974b9e630062c8c36e1670315ac
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2020-11-29 03:04:45 +0300

    ✨ Prototype of TeAppApi class

---
+++ shared/src/commonMain/kotlin/TeAppApi.kt
@@ -0,0 +1,18 @@
+import io.ktor.client.*
+import io.ktor.client.engine.*
+import io.ktor.client.request.*
+import kotlin.collections.get
+
+class TeAppApi(private val engine: HttpClientEngine) {
+    private val client = HttpClient()
+
+    suspend fun allTeaHouses(): String {
+        return client.get<String> {
+            url("$baseUrl/api/teahouses/all")
+        }
+    }
+
+    companion object {
+        private const val baseUrl = "https://teappp.herokuapp.com"
+    }
+}
