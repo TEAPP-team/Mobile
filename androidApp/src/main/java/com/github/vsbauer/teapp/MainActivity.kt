76c2aee1c09f5f9b118ede3bfc418df8bcba5236
vsbauer <vlad.bauer24@gmail.com>
2020-12-04 15:09:42 +0300

    Remove govnokod

--- androidApp/src/main/java/com/github/vsbauer/teapp/MainActivity.kt
+++ androidApp/src/main/java/com/github/vsbauer/teapp/MainActivity.kt
@@ -1,19 +1,5 @@
 package com.github.vsbauer.teapp
 
-import TeappApi
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
-class MainActivity : AppCompatActivity(R.layout.activity_main){
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        val api = TeappApi()
-        CoroutineScope(Dispatchers.IO).launch {
-            val res = api.allTeaHouses()
-            println("!!!!!!$res")
-        }
-    }
-}
+class MainActivity : AppCompatActivity(R.layout.activity_main)
