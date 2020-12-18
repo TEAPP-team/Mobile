37e9db22053afd230e299830710a5409be375ab4
VITYQ <vitaly.sorokin14@gmail.com>
2020-12-18 21:55:10 +0300

    Удален ненужный onCreate

--- androidApp/src/main/java/com/github/vsbauer/teapp/SplashscreenFragment.kt
+++ androidApp/src/main/java/com/github/vsbauer/teapp/SplashscreenFragment.kt
@@ -9,11 +9,6 @@ import android.view.ViewGroup
 
 class SplashscreenFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
