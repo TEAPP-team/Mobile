6807fb0c0a594b0cc2aeda7fbb8ffe304955b545
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2020-12-22 04:17:27 +0300

    Added teahouse data to FavouritesFragment.kt

--- androidApp/src/main/java/com/github/vsbauer/teapp/FavouritesFragment.kt
+++ androidApp/src/main/java/com/github/vsbauer/teapp/FavouritesFragment.kt
@@ -1,10 +1,15 @@
 package com.github.vsbauer.teapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import data.TeaHouse
+import kotlinx.android.synthetic.main.fragment_favourites.*
+import kotlinx.android.synthetic.main.teahouse_list.*
 
 
 class FavouritesFragment : Fragment() {
@@ -17,5 +22,48 @@ class FavouritesFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_favourites, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        updateData()
+    }
 
+    private fun updateData() {
+        val teahouseList = listOf(
+            TeaHouse(
+                title = "Чайный дом",
+                address = "Улица Рубинштейна, 13",
+                phone = "+79650097399",
+                site = "https://teahouse-group.ru/grand"
+            ), TeaHouse(
+                title = "Чайная студия на Васильевском",
+                address = "7-я линия Васильевского острова, д. 34",
+                phone = "+78129244385",
+                site = "https://kitchai.ru/"
+            ), TeaHouse(
+                title = "Белая обезьяна",
+                address = "ул. Малая Пушкарская, д. 4-6",
+                phone = "+78126282034",
+                site = "https://whitemonkeytea.ru"
+            )
+        )
+        teahouseList.forEach {
+            val newTeahouse = LayoutInflater.from(requireActivity().applicationContext)
+                .inflate(R.layout.favourite_list_item, null, false)
+
+            newTeahouse.findViewWithTag<TextView>("name").text = it.title
+
+            newTeahouse.setOnClickListener {
+                val teahouseIntent = Intent(
+                    requireActivity().applicationContext,
+                    TeahousePageActivity::class.java
+                )
+
+                teahouseIntent.putExtra("TeahouseData", it.toString())
+
+                startActivity(teahouseIntent)
+            }
+
+            favouriteListLayout.addView(newTeahouse)
+        }
+    }
 }
